6ccd084e4430c6dc8514e57efa67645b2ebef616
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-10-31 16:56:40 -0700

    modified display function to call create_csv

--- display_data.py
+++ display_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from fetch_data import fetch
+from create_csv import create_csv
 
 
 def display(fetch):
@@ -7,9 +8,11 @@ def display(fetch):
     data_frame = pd.DataFrame(
             data=covid_data, 
             columns=['Date', 'Country', 'Total Confirmed', 'Total Recovered', 'Total Deaths']
-        ) 
-
+          ) 
+    create_csv(data_frame)
     print(data_frame.head(20))
+    
     return data_frame
+
 
 display(fetch)
